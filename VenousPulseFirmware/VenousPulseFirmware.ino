8bb1a995c2e9f4fafb3700fc914c72d729cd4c3a
Jugularnauts <jugularnauts@gmail.com>
2017-11-16 17:16:47 -0600

    Replaced sample averaging with running average to smooth data. Need better SNR to avoid smoothing venous pulsation signal

--- VenousPulseFirmware/VenousPulseFirmware.ino
+++ VenousPulseFirmware/VenousPulseFirmware.ino
@@ -4,6 +4,7 @@
 // 11.6 updates: data acquisition with 2 accelerometers, 1-axis (Z)
 //                string w/ data sent to Processing for plotting
 // 11.14 updates: added data acquisition from pulse sensor
+// 11.16 updates: changed 50-sample average to moving average
 
 #include <math.h>  // include math library
 
@@ -44,71 +45,82 @@ int zRawMax2 = 631;
 // store data in string to send to Processing
 String Accel_string;
 
-// Take multiple samples to reduce noise
-const int sampleSize = 50;
+// Set sample size and initialize variables to store each sensor's data
+const int sampleSize = 5;
+int Z1s[sampleSize];
+int Z2s[sampleSize];
+int ECGs[sampleSize];
+int z1tot = 0;
+int z2tot = 0;
+int ecgtot = 0;
+int readIndex = 0;
+int z1avg = 0;
+int z2avg = 0;
+int ecgavg = 0;
+
 
 void setup() {
 // put your setup code here, to run once:
 analogReference(EXTERNAL);
 Serial.begin(9600);
+
+for (int thisSample = 0; thisSample<sampleSize; thisSample++) {
+  Z1s[thisSample] = 0;
+  Z2s[thisSample] = 0;
+  ECGs[thisSample] = 0;  
+}
+
 }
 
 void loop() {
 // put your main code here, to run repeatedly:
 
-// collect data samples
-int zRaw1 = ReadAxis(zInput1);
-int zRaw2 = ReadAxis(zInput2);
-int rawECG = ReadAxis(ECG_Input);
+// collect data sample, compute moving average
+z1tot = ReadAxisAvg(zInput1, Z1s, z1tot, readIndex);
+z1avg = z1tot/sampleSize;
+z2tot = ReadAxisAvg(zInput2, Z2s, z2tot, readIndex);
+z2avg = z2tot/sampleSize;
+ecgtot = ReadAxisAvg(ECG_Input, ECGs, ecgtot, readIndex);
+ecgavg = ecgtot/sampleSize;
 
-//Serial.println("Raw values");
-//Serial.print("Z-1: ");
-//Serial.println(zRaw1);
-//Serial.print(", Z-2: ");
-//Serial.println(zRaw2);
-//Serial.println();
+// advance to the next position in the array:
+readIndex = readIndex + 1;
+
+// if we're at the end of the array...
+if (readIndex >= sampleSize) {
+  // ...wrap around to the beginning:
+  readIndex = 0;
+}
 
 // Convert raw accel values to Gs
-long zScaled1 = map(zRaw1, zRawMin1, zRawMax1, -1000, 1000);
-long zScaled2 = map(zRaw2, zRawMin2, zRawMax2, -1000, 1000);
+long zScaled1 = map(z1avg, zRawMin1, zRawMax1, -1000, 1000);
+long zScaled2 = map(z2avg, zRawMin2, zRawMax2, -1000, 1000);
 
 // re-scale to fractional Gs
 float zAccel1 = zScaled1 / 1000.0;
 float zAccel2 = zScaled2 / 1000.0;
 
-//Serial.println("Acceleration");
-//Serial.print("Z-1: ");
-//Serial.print(zAccel1);
-//Serial.print("G");
-//Serial.print(", Z-2: ");
-//Serial.print(zAccel2);
-//Serial.println("G");
-//Serial.println();
-
 //delay(50);
 
-//if (!isnan(zAccel1) && !isnan(zAccel2) && !isnan(rawECG)) { 
-
 // send data over to Processing
-Accel_string = String(zAccel1) + ";" + String(zAccel2) + ";" + String(rawECG);
+Accel_string = String(zAccel1) + ";" + String(zAccel2) + ";" + String(ecgavg);
 Serial.print(Accel_string);
 Serial.print('\n');
 
-//}
 }
 
-//
-// Read "sampleSize" samples and report the average
-//
-int ReadAxis(int axisPin)
-{
-long reading = 0;
-analogRead(axisPin);
-delay(1);
-for (int i = 0; i < sampleSize; i++)
-{
-  reading += analogRead(axisPin);
-}
-return reading/sampleSize;
-}
+// Read running average of "sampleSize" samples, report the average
+int ReadAxisAvg(int pin, int readings[], int tot, int index)
+  {
+    // subtract the oldest reading:
+    tot = tot - readings[index];
+    // read from the sensor:
+    readings[index] = analogRead(pin);
+    // add the reading to the total:
+    tot = tot + readings[index];
+
+    delay(1);        // delay in between reads for stability
+    return(tot);
+  }
+  
 
